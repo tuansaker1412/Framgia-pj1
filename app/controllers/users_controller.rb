6ff92f434c3a215d1aa13cab150cf1cc99a17ec8
Ngoc Tuan <tuansaker1412@gmail.com>
2017-06-22 15:01:23 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:green] = "Created User!"
+      redirect_to @user
+    else
+      flash.now[:red] = "Error"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
